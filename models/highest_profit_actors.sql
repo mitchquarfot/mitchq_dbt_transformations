95044e3606a4ee26e2ba70aa4a1b43dbe72e7e1c
Mitch Quarfot <mitch.quarfot@fivetran.com>
2022-11-03 17:57:03 -0600

    highest profit model updates

--- models/highest_profit_actors.sql
+++ models/highest_profit_actors.sql
@@ -1,12 +1,25 @@
-select person_name, listagg(distinct title,', ') within group (order by title asc), (sum(revenue)/sum(budget)) as profit_ratio
+-- select person_name, listagg(distinct title,', ') within group (order by title asc), (sum(revenue)/sum(budget)) as profit_ratio
+-- from
+-- (select a.movie_id, title, b.person_id, person_name, cast_order, concat(a.movie_id, b.person_id, b.cast_order) as appearance_id, revenue, budget
+-- from mitch.GCP_SQLSERVER_TELEPORT_DEMO_MOVIES_DBO.movie a 
+-- join mitch.GCP_SQLSERVER_TELEPORT_DEMO_MOVIES_DBO.movie_cast b on a.movie_id = b.movie_id
+-- join mitch.GCP_SQLSERVER_TELEPORT_DEMO_MOVIES_DBO.person c on b.person_id = c.person_id
+-- where budget != 0
+-- and cast_order = 0
+-- group by 1,2,3,4,5,6,7,8)
+-- group by 1
+-- having count(distinct appearance_id) >= 5
+-- order by profit_ratio desc
+
+select agg.person_name, listagg(distinct agg.title,', ') within group (order by agg.title asc), (sum(agg.revenue)/sum(agg.budget)) as profit_ratio
 from
-(select a.movie_id, title, b.person_id, person_name, cast_order, concat(a.movie_id, b.person_id, b.cast_order) as appearance_id, revenue, budget
-from mitch.GCP_SQLSERVER_TELEPORT_DEMO_MOVIES_DBO.movie a 
-join mitch.GCP_SQLSERVER_TELEPORT_DEMO_MOVIES_DBO.movie_cast b on a.movie_id = b.movie_id
-join mitch.GCP_SQLSERVER_TELEPORT_DEMO_MOVIES_DBO.person c on b.person_id = c.person_id
+(select a.movie_id, a.title, b.person_id, c.person_name, b.cast_order, concat(a.movie_id, b.person_id, b.cast_order) as appearance_id, a.revenue, a.budget
+from "MITCH"."GCP_SQLSERVER_TELEPORT_DEMO_MOVIES_DBO"."MOVIE" a 
+join "MITCH"."GCP_SQLSERVER_TELEPORT_DEMO_MOVIES_DBO"."MOVIE_CAST" b on a.movie_id = b.movie_id
+join "MITCH"."GCP_SQLSERVER_TELEPORT_DEMO_MOVIES_DBO"."PERSON" c on b.person_id = c.person_id
 where budget != 0
 and cast_order = 0
-group by 1,2,3,4,5,6,7,8)
+group by 1,2,3,4,5,6,7,8) agg
 group by 1
 having count(distinct appearance_id) >= 5
-order by profit_ratio desc;
+order by profit_ratio desc
